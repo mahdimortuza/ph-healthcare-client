993e31d55d6b7aae2286b8f3b9619b8c7653d338
mahdimortuza <mahdi.webx@gmail.com>
2024-03-19 14:09:25 +0600

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@mui/material";
+
+const HomePage = () => {
+  return (
+    <div>
+      HomePage
+      <Button variant="text">Text</Button>
+    </div>
+  );
+};
+
+export default HomePage;
