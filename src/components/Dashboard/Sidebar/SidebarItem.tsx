c91544cffb33d22e86fba35f2cc17625b3010095
mahdimortuza <mahdi.webx@gmail.com>
2024-04-04 09:59:32 +0600

    active link added

--- src/components/Dashboard/Sidebar/SidebarItem.tsx
+++ src/components/Dashboard/Sidebar/SidebarItem.tsx
@@ -1,29 +1,38 @@
 import { DrawerItem } from "@/types";
 import {
-  default as InboxIcon,
-  default as MailIcon,
-} from "@mui/icons-material/Mail";
-import {
   ListItem,
   ListItemButton,
   ListItemIcon,
   ListItemText,
 } from "@mui/material";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 type IProps = {
   item: DrawerItem;
-  index: number;
 };
 
-const SidebarItem = ({ item, index }: IProps) => {
+const SidebarItem = ({ item }: IProps) => {
+  const linkPath = `/dashboard/${item.path}`;
+  const pathname = usePathname();
   return (
-    <Link href="/">
-      <ListItem key={index} disablePadding>
+    <Link href={linkPath}>
+      <ListItem
+        disablePadding
+        sx={{
+          ...(pathname === linkPath
+            ? {
+                borderRight: "3px solid #1586FD",
+                "& svg": {
+                  color: "#1586FD",
+                },
+              }
+            : {}),
+          mb: 1,
+        }}
+      >
         <ListItemButton>
-          <ListItemIcon>
-            {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-          </ListItemIcon>
+          <ListItemIcon>{item.icon && <item.icon></item.icon>}</ListItemIcon>
           <ListItemText primary={item.title} />
         </ListItemButton>
       </ListItem>
