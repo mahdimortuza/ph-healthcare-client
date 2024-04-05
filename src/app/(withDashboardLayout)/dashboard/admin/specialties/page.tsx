a949e8d25f9dbac21d9ad3c27317de2558149d9a
mahdimortuza <mahdi.webx@gmail.com>
2024-04-05 15:51:13 +0600

    render cell added

--- src/app/(withDashboardLayout)/dashboard/admin/specialties/page.tsx
+++ src/app/(withDashboardLayout)/dashboard/admin/specialties/page.tsx
@@ -4,10 +4,23 @@ import { Box, Button, Stack, TextField } from "@mui/material";
 import { useState } from "react";
 import SpecialtyModal from "./components/SpecialistModal";
 
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
+
 const SpecialtiesPage = () => {
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
   const { data, isLoading } = useGetAllSpecialtiesQuery({});
 
+  const columns: GridColDef[] = [
+    { field: "title", headerName: "Title", width: 100 },
+    {
+      field: "icon",
+      headerName: "Icon",
+      width: 100,
+      renderCell: ({ row }) => {
+        return console.log(row);
+      },
+    },
+  ];
   return (
     <Box>
       <Stack direction="row" justifyContent="space-between" alignItems="center">
@@ -15,6 +28,9 @@ const SpecialtiesPage = () => {
         <SpecialtyModal open={isModalOpen} setOpen={setIsModalOpen} />
         <TextField size="small" placeholder="Search Specialist" />
       </Stack>
+      <Box>
+        {!isLoading ? <DataGrid rows={data} columns={columns} /> : "Loading"}
+      </Box>
     </Box>
   );
 };
