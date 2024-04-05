527a1f92fedfdbe13d492a6caca7a5fdc52530bd
mahdimortuza <mahdi.webx@gmail.com>
2024-04-05 09:53:26 +0600

    modal added

--- src/app/(withDashboardLayout)/dashboard/admin/specialties/page.tsx
+++ src/app/(withDashboardLayout)/dashboard/admin/specialties/page.tsx
@@ -1,5 +1,16 @@
+import PHModal from "@/components/Shared/PHModal/PHModal";
+import { Box, Button, Stack, TextField } from "@mui/material";
+
 const SpecialtiesPage = () => {
-  return <div>SpecialtiesPage</div>;
+  return (
+    <Box>
+      <Stack direction="row" justifyContent="space-between" alignItems="center">
+        <Button>Create Specialty</Button>
+        <PHModal />
+        <TextField size="small" placeholder="Search Specialist" />
+      </Stack>
+    </Box>
+  );
 };
 
 export default SpecialtiesPage;
